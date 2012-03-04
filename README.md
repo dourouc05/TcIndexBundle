1a7b897082c9ab56392dfdc2110648f57e805da3
Thibaut Cuvelier <dourouc05@gmail.com>
2012-03-04 21:41:36 +0100

    First entities.

---
+++ README.md
@@ -0,0 +1,20 @@
+TCIndexBundle
+=============
+
+Three entitites at the root of everything: 
+### Section: a section of your page, presentation of the author e.g.
+### Category: a category of items, projects or articles e.g.
+### Item: an item, a projet or article e.g. 
+
+The resulting page will look like this: 
+
+    <template>
+	<section 1>
+	<section 2>
+	<category 1>
+		<item 1>
+		<item 2>
+	<category 2>
+		<item 1>
+		<item 2>
+	<template>
---
+++ src/TC/IndexBundle/Entity/Item.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace TC\IndexBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * TC\IndexBundle\Entity\Item
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Item
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string $url
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
