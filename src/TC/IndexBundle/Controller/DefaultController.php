b499479e0bdbfcc44c233ba0b9c972a8f3707b40
Thibaut Cuvelier <dourouc05@gmail.com>
2012-04-21 21:30:07 +0200

    First working prototype.

--- src/TC/IndexBundle/Controller/DefaultController.php
+++ src/TC/IndexBundle/Controller/DefaultController.php
@@ -9,6 +9,9 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TCIndexBundle:ThemeDefault:index.html.twig', array('name' => '4'));
+        $cats = $this->getDoctrine()
+                     ->getRepository('TCIndexBundle:Category')
+                     ->findByDepth(0);
+        return $this->render('TCIndexBundle:ThemeDefault:index.html.twig', array('categories' => $cats));
     }
 }
