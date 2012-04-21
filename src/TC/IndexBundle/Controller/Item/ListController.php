94cdd8e3585cb6d3e75f3fdefd0be2509c4acfe0
Thibaut Cuvelier <dourouc05@gmail.com>
2012-04-21 20:18:34 +0200

    Item sorting by category

--- src/TC/IndexBundle/Controller/Item/ListController.php
+++ src/TC/IndexBundle/Controller/Item/ListController.php
@@ -6,4 +6,19 @@ use Admingenerated\TCIndexBundle\BaseItemController\ListController as BaseListCo
 
 class ListController extends BaseListController
 {
+    protected function getQuery()
+    {
+        $query = $this->getDoctrine()
+                    ->getEntityManager()
+                    ->createQueryBuilder()
+                    ->select('i')
+                    ->from('TC\IndexBundle\Entity\Item', 'i')
+                    ->join('i.category', 'c')
+                    ->orderBy('c.order', 'ASC');
+
+        $this->processSort($query);
+        $this->processFilters($query);
+        
+        return $query->getQuery();
+    }
 }
