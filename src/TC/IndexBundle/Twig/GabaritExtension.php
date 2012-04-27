bbb509380697b3e4fb70584fed6f9fb5883cefd4
Thibaut Cuvelier <dourouc05@gmail.com>
2012-04-27 22:43:28 +0200

    Twig extension using database configuration.

--- src/TC/IndexBundle/Twig/GabaritExtension.php
+++ src/TC/IndexBundle/Twig/GabaritExtension.php
@@ -4,6 +4,7 @@ namespace TC\IndexBundle\Twig;
 
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
+use Symfony\Bundle\DoctrineBundle\Registry; 
 
 /**
  * A Twig extension for Developpez.com template. 
@@ -15,6 +16,18 @@ use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader;
  */
 class GabaritExtension extends \Twig_Extension
 {
+    private $options; 
+    
+    function __construct(Registry $doctrine) {
+        $options = $doctrine->getRepository('TCIndexBundle:Configuration')
+                            ->findAll(); 
+        
+        $this->options = array(); 
+        foreach($options as $o) {
+            $this->options[$o->getField()] = $o->getValue();
+        }
+    }
+    
     /**
      * Returns the name of the extension.
      *
@@ -22,7 +35,7 @@ class GabaritExtension extends \Twig_Extension
      */
     public function getName()
     {
-        return 'gabarit4';
+        return 'gabarit';
     }
 
     /**
@@ -39,9 +52,11 @@ class GabaritExtension extends \Twig_Extension
     public function gabUp()
     {
         ob_start();
-        $rubrique = 65; 
+        $rubrique = $this->options['rubrique_id']; 
         $gabarit_utf8 = false; 
-        $titre_page = $meta_description = $meta_keywords = 'Index'; 
+        $titre_page = $this->options['titre']; 
+        $meta_description = $this->options['description']; 
+        $meta_keywords = $this->options['mots-cles']; 
         $urlCss = './css.css';
         include $_SERVER['DOCUMENT_ROOT'] . '/template/entete.php'; 
         return utf8_encode(ob_get_clean()); 
@@ -50,11 +65,11 @@ class GabaritExtension extends \Twig_Extension
     public function gabDown()
     {
         ob_start();
-        $rubrique = 65; 
-        $fichierCachePied = $_SERVER['DOCUMENT_ROOT'] . '/template/caches/piedxhtml65.cache'; 
-        $Licence = '1'; 
+        $rubrique = $this->options['rubrique_id']; 
+        $fichierCachePied = $_SERVER['DOCUMENT_ROOT'] . '/template/caches/piedxhtml' . $rubrique . '.cache'; 
+        $Licence = $this->options['licence']; 
         $gabarit_utf8 = true; 
-        $Auteur = 'Thibaut Cuvelier';
+        $Auteur = $this->options['licence_auteur']; 
         $Annee = date('Y'); 
         include $_SERVER['DOCUMENT_ROOT'] . '/template/pied.php'; 
         return ob_get_clean(); 
