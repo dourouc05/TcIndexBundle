7c8bc89d0a78a05c609441df548c6927266694ad
Thibaut Cuvelier <dourouc05@gmail.com>
2012-05-04 21:26:09 +0200

    It's better when only useless files are deleted ^^.

---
+++ src/TC/IndexBundle/Controller/ImporterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace TC\IndexBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request; 
+use Symfony\Component\HttpFoundation\Response;
+use TC\IndexBundle\Entity\Category;
+use TC\IndexBundle\Entity\Item;
+use TC\IndexBundle\Importer\HtmlFileImporter;
+use TC\IndexBundle\Importer\XmlArticleImporter;
+use TC\IndexBundle\Importer\XmlCategoryImporter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Description of ImporterController
+ *
+ * @author Thibaut
+ */
+class ImporterController extends Controller {
+    /**
+     * @Route("/") 
+     */
+    public function indexAction() {
+        return $this->render('TCIndexBundle:Default:importers.html.twig');
+    }
+    
+    /**
+     * @Route("/html") 
+     */
+    public function htmlFileImportAction(Request $request) {
+        if ($request->getMethod() == 'GET') {
+            $importer = new HtmlFileImporter($this->getDoctrine()->getEntityManager()); 
+            $importer->import($_SERVER['DOCUMENT_ROOT'] . '/' . $_GET['page']); 
+            return $this->render('TCIndexBundle:Default:htmlFileImporter.html.twig', array('page' => $_GET['page']));
+        }
+        
+        throw new \Exception('Would you have tried to get here by your own means?');
+    }
+    
+    /**
+     * @Route("/xml/categories") 
+     */
+    public function xmlCategoriesImportAction() {
+        $importer = new XmlCategoryImporter($this->getDoctrine()->getEntityManager(), $_SERVER['DOCUMENT_ROOT']); 
+        $importer->import('tutoriels'); 
+        return $this->render('TCIndexBundle:Default:xmlCategoryImporter.html.twig');
+    }
+    
+    /**
+     * @Route("/xml/articles") 
+     */
+    public function xmlArticlesImportAction() {
+        $importer = new XmlArticleImporter($this->getDoctrine()->getEntityManager(), 'C:\\Program Files (x86)\\EasyPHP-5.3.8.0\\www\\index'); 
+        $importer->importFolder('tutoriels'); 
+        return $this->render('TCIndexBundle:Default:xmlArticleImporter.html.twig');
+    }
+}
