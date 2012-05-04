ecea78c82fa60d25447328991800177accd575ff
Thibaut Cuvelier <dourouc05@gmail.com>
2012-05-04 21:21:02 +0200

    Code simplification.

--- src/TC/IndexBundle/Controller/Category/ListController.php
+++ src/TC/IndexBundle/Controller/Category/ListController.php
@@ -3,9 +3,13 @@
 namespace TC\IndexBundle\Controller\Category;
 
 use Admingenerated\TCIndexBundle\BaseCategoryController\ListController as BaseListController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ListController extends BaseListController
 {
+    private $em; 
+    private $q; 
+    
     protected function getQuery()
     {
         $query = $this->getDoctrine()
@@ -19,5 +23,42 @@ class ListController extends BaseListController
         $this->processFilters($query);
         
         return $query->getQuery();
+    }
+
+    /**
+     * @Route("/{pk}/move-up")
+     */
+    public function upAction($pk)
+    {
+        $id = $pk; 
+        $this->getResult($id); 
+        $this->q->pushAbove(); 
+        return $this->end(); 
+    }
+
+    /**
+     * @Route("/{pk}/move-down")
+     */
+    public function downAction($pk)
+    {
+        $id = $pk; 
+        $this->getResult($id); 
+        $this->q->pushBelow(); 
+        return $this->end(); 
+    }
+    
+    private function getResult($id) {
+        $this->em = $this->getDoctrine()->getEntityManager(); 
+        $this->q  = $this->em->createQuery('SELECT c FROM TCIndexBundle:Category c WHERE c.id = :id')
+                         ->setParameter('id', $id)
+                         ->getSingleResult();
+    }
+    
+    private function end() {
+        $this->q->prePersist($this->em);
+        $this->em->persist($this->q);
+        $this->em->flush();
+        
+        return $this->redirect($this->generateUrl('TC_IndexBundle_Category_list'));
     }
 }
--- src/TC/IndexBundle/Controller/CategoryMoverController.php
+++ src/TC/IndexBundle/Controller/CategoryMoverController.php
@@ -7,37 +7,4 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CategoryMoverController extends Controller
 {
-    private $em; 
-    private $q; 
-
-    public function upAction($pk)
-    {
-        $id = $pk; 
-        $this->getResult($id); 
-        $this->q->pushAbove(); 
-        return $this->end(); 
-    }
-    
-    public function downAction($pk)
-    {
-        $id = $pk; 
-        $this->getResult($id); 
-        $this->q->pushBelow(); 
-        return $this->end(); 
-    }
-    
-    private function getResult($id) {
-        $this->em = $this->getDoctrine()->getEntityManager(); 
-        $this->q  = $this->em->createQuery('SELECT c FROM TCIndexBundle:Category c WHERE c.id = :id')
-                         ->setParameter('id', $id)
-                         ->getSingleResult();
-    }
-    
-    private function end() {
-        $this->q->prePersist($this->em);
-        $this->em->persist($this->q);
-        $this->em->flush();
-        
-        return $this->redirect($this->generateUrl('TC_IndexBundle_Category_list'));
-    }
 }
